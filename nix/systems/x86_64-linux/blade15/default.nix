577c7282e067b590d97a067784cca2c76aa52f75
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-01-10 20:39:40 +0100

    chore: clean useless comments

--- nix/systems/x86_64-linux/blade15/default.nix
+++ nix/systems/x86_64-linux/blade15/default.nix
@@ -1,14 +1,9 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
   lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
   pkgs,
-  # You also have access to your flake's inputs.
   inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
+  namespace,
+  system,
   ...
 }: {
   imports = [
