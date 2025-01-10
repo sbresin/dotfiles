5712f683a9404f07da985dbc3a6d5360e915cc1f
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-01-10 20:36:01 +0100

    enhance: use kanata config from flake source directly

--- nix/systems/x86_64-linux/blade15/kanata.nix
+++ nix/systems/x86_64-linux/blade15/kanata.nix
@@ -1,21 +1,13 @@
-{
-  lib,
-  pkgs,
-  ...
-}: let
-  # copy config files to nix store
-  kanata-config = pkgs.stdenvNoCC.mkDerivation {
-    name = "kanata-config";
-    src = lib.snowfall.fs.get-file "dot-config/kanata";
-    postInstall = ''
-      mkdir $out
-      cp -v *.kbd $out
-    '';
-  };
+{lib, ...}: let
+  # use config files from flake repo
+  config-dir = lib.snowfall.fs.get-file "stow/dot-config/kanata";
 in {
   hardware.uinput.enable = true;
-  services.kanata.enable = true;
-  services.kanata.keyboards."all".configFile = "${kanata-config}/kanata.kbd";
+
+  services.kanata = {
+    enable = true;
+    keyboards."all".configFile = "${config-dir}/kanata.kbd";
+  };
   systemd.services.kanata-all.serviceConfig = {
     # breaks the service for me, PermissionDenied trying to register any keyboard input device
     DynamicUser = lib.mkForce false;
