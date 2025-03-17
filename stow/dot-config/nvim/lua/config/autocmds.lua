5702e8d6479987d18562057c0021515e7ef8f4be
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-03-17 11:29:22 +0100

    fix(nvim): restart prettierd on config changes

--- stow/dot-config/nvim/lua/config/autocmds.lua
+++ stow/dot-config/nvim/lua/config/autocmds.lua
@@ -1,4 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
+-- restart prettierd after config file save
+vim.api.nvim_create_autocmd({'BufWritePost'}, {
+    group = vim.api.nvim_create_augroup('RestartPrettierd', {clear = true}),
+    pattern = '*prettier*',
+    callback = function() vim.fn.system('prettierd restart') end
+})
