8616c2b5857b8a385952f622412d679b467bd12a
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-03-27 20:22:02 +0100

    fix: xonsh explicitly include prompt-toolkit

--- nix/packages/xonsh/default.nix
+++ nix/packages/xonsh/default.nix
@@ -4,20 +4,22 @@ in
   channels.nixpkgs-unstable.xonsh.override {
     python3 = channels.nixpkgs-unstable.python313;
     extraPackages = pythonPackages:
-      with xontribs {inherit pythonPackages;}; [
-        xonsh-direnv
-        xontrib-abbrevs
-        xontrib-argcomplete
-        xontrib-clp
-        xontrib-cmd-durations
-        xontrib-fzf-completions
-        xontrib-jedi
-        xontrib-prompt-bar
-        xontrib-prompt-starship
-        xontrib-readable-traceback
-        xontrib-sh
-        xontrib-term-integrations
-        xontrib-vox
-        xontrib-whole-word-jumping
-      ];
+      with xontribs {inherit pythonPackages;};
+        [
+          xonsh-direnv
+          xontrib-abbrevs
+          xontrib-argcomplete
+          xontrib-clp
+          xontrib-cmd-durations
+          xontrib-fzf-completions
+          xontrib-jedi
+          xontrib-prompt-bar
+          xontrib-prompt-starship
+          xontrib-readable-traceback
+          xontrib-sh
+          xontrib-term-integrations
+          xontrib-vox
+          xontrib-whole-word-jumping
+        ]
+        ++ [pythonPackages.prompt-toolkit];
   }
