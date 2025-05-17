ff47e9845d3255267c39c16edc0554a129064751
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-05-17 11:42:55 +0200

    enhance: better cachix setup

--- nix/modules/nixos/cachix/default.nix
+++ nix/modules/nixos/cachix/default.nix
@@ -3,13 +3,17 @@
     trusted-users = ["sebe"];
 
     # the system-level substituters & trusted-public-keys
-    substituters = [
-      "https://cache.nixos.org"
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
     ];
-
-    # trusted-public-keys = [
-    #   # the default public key of cache.nixos.org, it's built-in, no need to add it here
-    #   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    # ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://wezterm.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://anyrun.cachix.org"
+    ];
   };
 }
