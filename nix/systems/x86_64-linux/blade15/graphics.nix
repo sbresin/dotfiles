440d329f681aa100782adaf21bdc3c6ab99afea8
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-05-21 22:21:18 +0200

    feat: add various nvidia prime related specializations for testing

--- nix/systems/x86_64-linux/blade15/graphics.nix
+++ nix/systems/x86_64-linux/blade15/graphics.nix
@@ -25,8 +25,13 @@
     ];
   };
 
+  environment.systemPackages = with pkgs; [
+    # useful for debugging PRIME problems
+    mesa-demos
+    vulkan-tools
+  ];
+
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
     open = true;
     modesetting.enable = true;
     nvidiaSettings = true;
@@ -40,16 +45,32 @@
         enableOffloadCmd = true;
       };
     };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
-  specialisation = {
-    nvidia.configuration = {
-      hardware.nvidia = {
-        powerManagement.enable = lib.mkForce true;
-        powerManagement.finegrained = lib.mkForce false;
-        prime.offload.enable = lib.mkForce false;
-        prime.offload.enableOffloadCmd = lib.mkForce false;
-      };
+  specialisation.nvidia-sync.configuration = {
+    environment.etc."specialisation".text = "nvidia-sync";
+    environment.sessionVariables.PRIME_MODE = "sync";
+    hardware.nvidia = {
+      powerManagement.finegrained = lib.mkForce false;
+      prime.offload.enable = lib.mkForce false;
+      prime.offload.enableOffloadCmd = lib.mkForce false;
+      prime.sync.enable = true;
+    };
+  };
+  specialisation.nvidia-reverse.configuration = {
+    environment.etc."specialisation".text = "nvidia-reverse";
+    environment.sessionVariables.PRIME_MODE = "reverse";
+    hardware.nvidia.prime.reverseSync.enable = true;
+  };
+  specialisation.nvidia-discrete.configuration = {
+    environment.etc."specialisation".text = "nvidia-discrete";
+    environment.sessionVariables.PRIME_MODE = "discrete";
+    hardware.nvidia = {
+      powerManagement.enable = lib.mkForce true;
+      powerManagement.finegrained = lib.mkForce false;
+      prime.offload.enable = lib.mkForce false;
+      prime.offload.enableOffloadCmd = lib.mkForce false;
     };
   };
 }
