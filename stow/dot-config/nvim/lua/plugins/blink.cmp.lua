abe7c3879b5eb917f4fca97ea66f6d042232f4a9
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-05-23 13:23:58 +0200

    feat: put copilot suggestions below LSP ones

--- stow/dot-config/nvim/lua/plugins/blink.cmp.lua
+++ stow/dot-config/nvim/lua/plugins/blink.cmp.lua
@@ -1,6 +1,10 @@
 return {
     {
         "saghen/blink.cmp",
-        opts = function(_, opts) opts.signature = {enabled = true} end
+        opts = function(_, opts)
+            opts.signature = opts.signature or {}
+            opts.signature.enabled = true -- enable experimental signature help
+            opts.sources.providers.copilot.score_offset = -1 -- put copilot below LSP
+        end
     }
 }
