08e73d3ee5956c3280487e1518279d2107c7c8a7
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-05-26 08:40:34 +0200

    enhance: try to hide tuigreet mouse

--- nix/systems/x86_64-linux/blade15/greetd.nix
+++ nix/systems/x86_64-linux/blade15/greetd.nix
@@ -8,19 +8,20 @@
   cage = "${pkgs.cage}/bin/cage";
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
   alacritty = "${pkgs.alacritty}/bin/alacritty";
-  alacritty_options = "--option 'window.decorations=\"None\"' --option 'window.startup_mode=\"Fullscreen\"' --option 'font.size=16' --option 'colors.primary.background=\"#191724\"'";
-  theme = "text=#E0DEF4;container=#1f1d2e;border=#6e6a86;title=#9ccfd8;prompt=#ebbcba;input=#eb6f92;time=#ebbcba";
+  alacritty_options = "--option 'window.decorations=\"None\"' --option 'window.startup_mode=\"Fullscreen\"' --option 'font.size=18' --option 'colors.primary.background=\"#191724\"' --option 'mouse.hide_when_typing=true'";
+  theme = "text=#e0def4;container=#1f1d2e;border=#6e6a86;title=#9ccfd8;prompt=#ebbcba;input=#eb6f92;time=#ebbcba";
 in {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${cage} -dms -- ${alacritty} ${alacritty_options} --command ${tuigreet} --remember --remember-session --time --window-padding 2 --theme '${theme}'";
+        command = "${cage} -ds -m last -- ${alacritty} ${alacritty_options} --command ${tuigreet} --remember --remember-session --time --window-padding 2 --theme '${theme}'";
       };
     };
   };
 
-  systemd.services.greetd.serviceConfig = {
-    Environment = "XCURSOR_PATH=${transparent-cursor}/Transparent";
+  systemd.services.greetd.environment = {
+    XCURSOR_PATH = transparent-cursor;
+    XCURSOR_THEME = "Transparent";
   };
 }
