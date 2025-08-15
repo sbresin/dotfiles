f6ea4a6ceec473bf8bfd84feef4106c1666fc9e5
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-08-15 15:23:50 +0200

    feat(nvim): set default tabstop in absence of editorconfig

--- stow/dot-config/nvim/lua/config/options.lua
+++ stow/dot-config/nvim/lua/config/options.lua
@@ -1,16 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 -- Apex file types
 vim.filetype.add({
-	extension = {
-		cls = "apexcode",
-		apex = "apexcode",
-		trigger = "apexcode",
-		soql = "soql",
-		sosl = "sosl",
-	},
+    extension = {
+        cls = "apexcode",
+        apex = "apexcode",
+        trigger = "apexcode",
+        soql = "soql",
+        sosl = "sosl"
+    }
 })
 
-vim.treesitter.language.register("apex", { "apexcode" })
+vim.treesitter.language.register("apex", {"apexcode"})
+
+-- anything less would be insane
+vim.opt.tabstop = 4;
