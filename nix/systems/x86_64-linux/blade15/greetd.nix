e2999f7701639f2e798217c9c2dbfb4c73036cd8
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-10-27 14:39:12 +0100

    fix: use foot instead of alacritty for tuigreetd

--- nix/systems/x86_64-linux/blade15/greetd.nix
+++ nix/systems/x86_64-linux/blade15/greetd.nix
@@ -7,15 +7,34 @@
   };
   cage = "${pkgs.unstable.cage}/bin/cage";
   tuigreet = "${pkgs.unstable.tuigreet}/bin/tuigreet";
-  alacritty = "${pkgs.unstable.alacritty}/bin/alacritty";
-  alacritty_options = "--option 'window.decorations=\"None\"' --option 'window.startup_mode=\"Fullscreen\"' --option 'font.size=18' --option 'colors.primary.background=\"#191724\"' --option 'mouse.hide_when_typing=true'";
+  foot = "${pkgs.unstable.foot}/bin/foot";
   theme = "text=#e0def4;container=#1f1d2e;border=#6e6a86;title=#9ccfd8;prompt=#ebbcba;input=#eb6f92;time=#ebbcba";
+  # kmscon = "${pkgs.kmscon}/bin/kmscon --font-dpi=153 --font-size=18 --no-reset-env --login --";
+  foot_conf = pkgs.writeTextFile {
+    name = "foot.ini";
+    text = ''
+      [main]
+      shell=${tuigreet} --remember --remember-session --time --window-padding 2 --theme '${theme}'
+
+      font=Dank Mono:size=18, Symbols Nerd Font Mono:size=18, Apple Color Emoji:size=18
+      font-bold=Dank Mono:size=18:weight=bold, Symbols Nerd Font Mono:size=18, Apple Color Emoji:size=18
+
+      pad=3x3 center-when-maximized-and-fullscreen
+
+      initial-window-mode=fullscreen
+
+      [colors]
+      background=191724
+      foreground=e0def4
+    '';
+  };
 in {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${cage} -ds -m last -- ${alacritty} ${alacritty_options} --command ${tuigreet} --remember --remember-session --time --window-padding 2 --theme '${theme}'";
+        command = "${cage} -ds -m last -- ${foot} --config=${foot_conf}";
+        # command = "${kmscon} ${tuigreet} --remember --remember-session --time --window-padding 2 --theme '${theme}'";
       };
     };
   };
