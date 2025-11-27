97c31015c18acb0e1dd47790f0dc47093b12837c
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-11-27 14:26:54 +0100

    feat(pi-server): install pi-hole in container

--- nix/systems/aarch64-linux/pi-server/pi-hole-container.nix
+++ nix/systems/aarch64-linux/pi-server/pi-hole-container.nix
@@ -1,11 +1,16 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   containers.pi-hole = {
     autoStart = true;
     ephemeral = true;
     macvlans = ["eth0"];
     privateNetwork = true;
     #extraFlags = ["-U"]; # private user namespace
-    config = {pkgs, ...}: {
+    config = {...}: {
+      nixpkgs.pkgs = pkgs; # use host packages
       networking = {
         hostName = "pi-hole";
         useDHCP = false;
@@ -27,6 +32,30 @@
           };
         };
       };
+
+      imports = [
+        "${inputs.nixpkgs-unstable}/nixos/modules/services/networking/pihole-ftl.nix"
+        "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/pihole-web.nix"
+      ];
+      services.pihole-ftl = {
+        enable = true;
+        package = pkgs.unstable.pihole-ftl;
+        piholePackage = pkgs.unstable.pihole;
+        openFirewallWebserver = true;
+        openFirewallDNS = true;
+        openFirewallDHCP = false;
+      };
+
+      services.pihole-web = {
+        enable = true;
+        package = pkgs.unstable.pihole-web;
+        ports = [
+          "80r"
+          "443s"
+        ];
+      };
+
+      system.stateVersion = "25.05";
     };
   };
 }
