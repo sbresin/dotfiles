320d8c7afc104290d9f079173e464807684dcb99
Sebastian Bresin <sebastian.bresin@gmail.com>
2025-11-29 12:51:18 +0100

    fix: don't set mimeapps for now

--- nix/homes/x86_64-linux/sebe@blade15/default.nix
+++ nix/homes/x86_64-linux/sebe@blade15/default.nix
@@ -1,22 +1,4 @@
-{
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  home, # The home architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
-  format, # A normalized name for the home target (eg. `home`).
-  virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
-  host, # The host name for this home.
-  # All other arguments come from the home home.
-  config,
-  ...
-}: {
+{...}: {
   home.username = "sebe";
   home.homeDirectory = "/home/sebe";
 
@@ -24,17 +6,18 @@
   programs.home-manager.enable = true;
 
   # set default apps
-  xdg.mimeApps = {
-    enable = true;
+  # xdg.mimeApps = {
+  #   enable = true;
+  #
+  #   defaultApplications = {
+  #     "text/html" = "io.github.zen_browser.zen.desktop";
+  #     "x-scheme-handler/http" = "io.github.zen_browser.zen.desktop";
+  #     "x-scheme-handler/https" = "io.github.zen_browser.zen.desktop";
+  #     "x-scheme-handler/about" = "io.github.zen_browser.zen.desktop";
+  #     "x-scheme-handler/unknown" = "io.github.zen_browser.zen.desktop";
+  #   };
+  # };
 
-    defaultApplications = {
-      "text/html" = "io.github.zen_browser.zen.desktop";
-      "x-scheme-handler/http" = "io.github.zen_browser.zen.desktop";
-      "x-scheme-handler/https" = "io.github.zen_browser.zen.desktop";
-      "x-scheme-handler/about" = "io.github.zen_browser.zen.desktop";
-      "x-scheme-handler/unknown" = "io.github.zen_browser.zen.desktop";
-    };
-  };
   # use bluetooth device buttons for media control
   services.mpris-proxy.enable = true;
 
