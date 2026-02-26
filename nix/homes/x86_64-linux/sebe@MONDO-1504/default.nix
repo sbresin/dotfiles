70467cfe5486395f1592e39deb5f7d528df00d79
Sebastian Bresin <sebastian.bresin@gmail.com>
2026-02-26 20:16:37 +0100

    fix: ddcutil hang, blacklist amdgpu i2c

--- nix/homes/x86_64-linux/sebe@MONDO-1504/default.nix
+++ nix/homes/x86_64-linux/sebe@MONDO-1504/default.nix
@@ -5,5 +5,14 @@
   # use bluetooth device buttons for media control
   services.mpris-proxy.enable = true;
 
+  # Tell libddcutil (used by ddcutil-service & vdu_controls) to skip the
+  # laptop's internal eDP panel I2C bus. Probing i2c-3 (AMDGPU DM i2c hw
+  # bus 0, wired to eDP-1) can deadlock the amdgpu driver and freeze the
+  # screen on this Framework AMD Strix Point laptop.
+  xdg.configFile."ddcutil/ddcutilrc".text = ''
+    [global]
+    options: --ignore-bus 3
+  '';
+
   home.stateVersion = "25.11";
 }
