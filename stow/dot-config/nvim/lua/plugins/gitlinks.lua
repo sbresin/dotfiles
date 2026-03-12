84bf4dbb4db1525749162a9321fb1d78aa6f2998
Sebastian Bresin <sebastian.bresin@gmail.com>
2026-03-12 21:21:57 +0100

    enhance: make gitlinks default to origin

--- stow/dot-config/nvim/lua/plugins/gitlinks.lua
+++ stow/dot-config/nvim/lua/plugins/gitlinks.lua
@@ -4,8 +4,8 @@ return {
 		cmd = "GitLink",
 		opts = {},
 		keys = {
-			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+			{ "<leader>gy", "<cmd>GitLink remote=origin<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink! remote=origin<cr>", mode = { "n", "v" }, desc = "Open git link" },
 		},
 	},
 }
